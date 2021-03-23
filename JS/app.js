d9c7a9342bcd68c5c78f037843df453961413ba3
t-gomb <trentgombas@gmail.com>
2021-03-23 16:39:30 -0700

    added_list_with_todo_and_status

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
-// your JavaScript goes here, you can delete this comment
+let todolist = [
+    {todo: 'clone repo', status: 'complete'},
+    {todo: 'link style sheet and JS to html', status: 'complete'},
+    {todo: 'commit messages', status: 'started'},
+    {todo: 'work on JS', status: 'started'}
+]
+
+let completedTodos = {}
+
+
+
+console.log(todolist)
